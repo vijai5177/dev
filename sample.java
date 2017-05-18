2152c8722feaf6ceb9f83e286c5582a44b61f1b7
vijai5177 <vijaikommineni@gmail.com>
2017-05-18 15:28:55 +0530

    second java program

--- sample.java
+++ sample.java
@@ -1,7 +1,17 @@
 
-public class HelloWorld
+public class sample
 {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
+
+	public static void main(String[] args)
+	{	final int NUM_FACTS = 100;
+		for(int i = 0; i < NUM_FACTS; i++)
+			System.out.println( i + "! is " + factorial(i));
+	}
+	
+	public static int factorial(int n)
+	{	int result = 1;
+		for(int i = 2; i <= n; i++)
+			result *= i;
+		return result;
 	}
 }
